dd0313a049467a430a2e68acb47712356c13adac
Jesse Mejia <jmejia@mercycorps.org>
2014-06-30 12:10:02 -0700

    added html interpolation + youtube embeds

--- jmej/recordfeed_simple.js
+++ jmej/recordfeed_simple.js
@@ -2,15 +2,20 @@ var http = require('http');
 var url = require('url');
 var interpolate = require('./interpolator');
 
-var records = [{discogs: "http://www.discogs.com/Stenny-Andrea-Vostok-Smokescreen/release/4940085", youtube: "9uR0LJ_m6rY"}, 
-{discogs: "http://www.discogs.com/Sue%C3%B1o-Latino-Luxuria/release/120181", youtube: "JL92HkP96uU"}] //this should pull from discogs in the next rev: http://www.discogs.com/developers/resources/user/wantlist.html
+var records = [{discogs: "http://www.discogs.com/Black-Dog-Virtual/release/881990", youtube: "https://www.youtube.com/watch?v=5BYlQW68l4w"}, 
+{discogs: "http://www.discogs.com/Sue%C3%B1o-Latino-Luxuria/release/120181", youtube: "https://www.youtube.com/watch?v=JL92HkP96uU"}] //this should pull from discogs in the next rev: http://www.discogs.com/developers/resources/user/wantlist.html
 
 //templates
 var headerPartial =  '<body><head><title>Record Feed</title></head><h1>Record Feed</h1>';
-var recordsPartial = '<ul><li><iframe width="420" height="315" src="//www.youtube.com/embed/{{youtube}}"frameborder="0" allowfullscreen></iframe></li><li><a href="{{discogs}}">click here for discogs release page</a></ul>';
+var recordsPartial = '<ul><li>name: {{name}} </li><li>age: {{age}} </ul>';
 var footerPartial =  '<footer>jams.</footer></body>';
 var recordsPage = '';
 
+//make youtube embed code
+function yt(url){
+return '<iframe width="420" height="315" src="'+url+
+'" frameborder="0" allowfullscreen></iframe>'
+}
 
 var server = http.createServer(function (req, res) {
 	var pathRequested = url.parse(req.url, true).pathname;
@@ -32,11 +37,11 @@ var server = http.createServer(function (req, res) {
     	recordsPage = headerPartial;
 
       	records.forEach(function(item, index) {
-        recordsPage += interpolate(recordsPartial, item);
+        recordsPage += (interpolate(recordsPartial, yt(item.youtube) +'\n'+ item.discogs+'\n'+'\n' ));
      		});
-      	      recordsPage += footerPartial;
+      	      contactsPage += footerPartial;
 
-      	res.writeHead({'Content-Type': 'text/plain'});
+      	res.writeHead({'Content-Type': 'text/html'});
       	res.end(recordsPage);
       	res.end();
       	break;
